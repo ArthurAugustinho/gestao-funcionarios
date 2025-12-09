d091ecb802b7e2c9ef1c29ebeab6b41c97bb1cce
ArthurAugustinho <arthuraugustinho35@gmail.com>
2025-12-09 12:49:05 -0300

    Configuração postgres, flyway e estrutura inicial de pacotes

--- src/main/java/com/ArthurAugustinho/gestaofuncionarios/GestaoFuncionariosApplication.java
+++ src/main/java/com/ArthurAugustinho/gestaofuncionarios/GestaoFuncionariosApplication.java
@@ -1,4 +1,4 @@
-package gestao_funcionarios;
+package com.ArthurAugustinho.gestaofuncionarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
