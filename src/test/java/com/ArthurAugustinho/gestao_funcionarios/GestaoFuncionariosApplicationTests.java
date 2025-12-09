ce51f9b6a191fd6aec9fee6212dda1e06db2544d
ArthurAugustinho <arthuraugustinho35@gmail.com>
2025-12-09 11:58:09 -0300

    Projeto base criado pelo Spring Initialzr

---
+++ src/test/java/com/ArthurAugustinho/gestao_funcionarios/GestaoFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArthurAugustinho.gestao_funcionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
